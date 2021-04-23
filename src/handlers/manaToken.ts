2784084423d65b571d5ca5794c551f2587d45f10
Ignacio Mazzara <nachomazzara@gmail.com>
2021-04-22 21:14:52 -0300

    fix: burn and mint

--- src/handlers/manaToken.ts
+++ src/handlers/manaToken.ts
@@ -1,4 +1,4 @@
-import { MANAToken, Transfer } from '../entities/ManaToken/ManaToken'
+import { MANAToken, Transfer, Mint, Burn } from '../entities/ManaToken/ManaToken'
 import { Account } from '../entities/schema'
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
@@ -19,6 +19,30 @@ export function handleTransfer(event: Transfer): void {
     let accountTo = new Account(accountToId)
 
     accountTo.mana = manaContract.balanceOf(event.params.to)
+    accountTo.save()
+  }
+}
+
+export function handleMint(event: Mint): void {
+  let manaContract = MANAToken.bind(event.address)
+
+  let accountToId = event.params.to.toHex()
+  if (accountToId != ZERO_ADDRESS) {
+    let accountTo = new Account(accountToId)
+
+    accountTo.mana = manaContract.balanceOf(event.params.to)
+    accountTo.save()
+  }
+}
+
+export function handleBurn(event: Burn): void {
+  let manaContract = MANAToken.bind(event.address)
+
+  let accountFromId = event.params.burner.toHex()
+  if (accountFromId != ZERO_ADDRESS) {
+    let accountTo = new Account(accountFromId)
+
+    accountTo.mana = manaContract.balanceOf(event.params.burner)
     accountTo.save()
   }
 }
